cfe60f72fef3e5cd401a8b81334eabd6e0136cc7
grace <lov011052@gmail.com>
2020-07-07 16:20:25 +0800

    0707恐怖遊戲練習

---
+++ Assets/Script/HorroObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class HorroObject : MonoBehaviour, ITiggerEvent
+{
+    /// <summary>
+    /// 允許子類別覆寫觸發事件
+    /// </summary>
+    /// <param name="other"></param>
+    public virtual void TriggerEvent()
+    {
+
+    } 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "玩家") TriggerEvent();
+    }
+
+}
